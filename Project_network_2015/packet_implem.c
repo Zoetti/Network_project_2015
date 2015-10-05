493a1a7f81c7b9837e4bc2dcf8973e7ca128c25c
Geoffroy Zoetardt <geoffroyzoetardt@wifi-studentl-6031.sri.ucl.ac.be>
2015-10-05 17:30:09 +0200

    test_ingi

--- Project_network_2015/packet_implem.c
+++ Project_network_2015/packet_implem.c
@@ -56,6 +56,55 @@ void pkt_del(pkt_t *pkt)
     free(pkt);
 }
 
+pkt_t* stock_in_packet(const char* data, const size_t len){
+    pkt_t* pkt = pkt_new();
+    uint8_t i = 0;
+    while (i<len){
+        if (i<3){
+            char * dst = (char*)malloc(sizeof(ptypes_t));
+            strncpy(dst, data, sizeof(ptypes_t));
+            pkt_set_type(pkt, (ptypes_t)dst);
+            i=i+sizeof(ptypes_t);
+            free(dst);
+        }
+        else if(i>=3 && i<8){
+            char * dst = (char*)malloc(sizeof(uint8_t));
+            strncpy(dst, data + i, sizeof(uint8_t));
+            pkt_set_window(pkt, (uint8_t)atoi(dst));
+            i=i+sizeof(uint8_t);
+            free(dst);
+        }
+        else if(i>=8 && i<16){
+            char * dst = (char*)malloc(sizeof(uint8_t));
+            strncpy(dst, data + i, sizeof(uint8_t));
+            pkt_set_seqnum(pkt, (uint8_t)atoi(dst));
+            i=i+sizeof(uint8_t);
+            free(dst);
+        }
+        else if(i>=16 && i<32){
+            char * dst = (char*)malloc(sizeof(uint16_t));
+            strncpy(dst, data + i, sizeof(uint16_t));
+            pkt_set_length(pkt, (uint8_t)atoi(dst));
+            i=i+sizeof(uint16_t);
+            free(dst);
+        }
+        else if(i>=32 && i<len-4){
+            char * dst = (char*)malloc(len-4-i);
+            strncpy(dst, data + i, len-4-i);
+            pkt_set_payload(pkt, dst,len-4-i);
+            i=len-4;
+            free(dst);
+        }
+        else{
+            char * dst = (char*)malloc(sizeof(uint32_t));
+            strncpy(dst, data + i, sizeof(uint32_t));
+            pkt_set_crc(pkt, (uint32_t) atoi(dst));
+            i=len;
+            free(dst);
+        }
+    }
+    return pkt;
+}
 /*
  * this function decode data that has been received and create a new pkt_t from it.
  * This functions performs the following checks:
@@ -63,20 +112,24 @@ void pkt_del(pkt_t *pkt)
  *   end of the data stream
  * - Validates the type of the packet
  * - Validates the length of the packet
+ * @data: The set of bytes that constitutes the packet
+ * @len: The number of bytes received
+ * @pkt: An allocated struct pkt, where the decoded data should be stored.
+ *
+ * @return: A status code indicating the success or the failure type.
+ * 			Unless the error is E_NOHEADER, the packet has at least the
+ * 			values of the header found in the data stream.
 */
 pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
 {
+    pkt = stock_in_packet(data, len);
     //Check the CRC
-    if(pkt->crc != crc32(0L,(const Bytef*) data,sizeof(pkt_t))) {
+    if(pkt->crc != crc32(0L,(const Bytef*) data,sizeof(uint32_t))) {
         return E_CRC;
     }
     //Check if the type is right
-    else if (pkt->type != PTYPE_DATA || pkt->type != PTYPE_ACK || pkt->type != PTYPE_NACK) {
+    else if (pkt->type != PTYPE_DATA && pkt->type != PTYPE_ACK && pkt->type != PTYPE_NACK) {
         return E_TYPE;
-    }
-    //Check the length
-    else if (!(0 <= pkt->length) || !(pkt->length <= 512)) {
-        return E_LENGTH;
     }
     else {
         return PKT_OK;
@@ -98,21 +151,22 @@ pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
 
 pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
 {
-    if (sizeof(pkt_t) > *len){
+    if (sizeof(pkt) > *len){
         return E_NOMEM;
     }
     //flux de bytes du payload et du header (sans le CRC)
-    char* pkt_bytes = (char*)malloc(sizeof(pkt_t*)-4);
+    char* pkt_bytes = (char*)malloc(sizeof(pkt)-4);
     if (pkt_bytes == NULL){
         fprintf(stderr,"Impossible allocation \n");
         return E_NOMEM;
     }
-    memcpy(pkt_bytes, pkt, sizeof(pkt_t)-4);
+    memcpy(pkt_bytes, pkt, sizeof(pkt)-4);
     uLong crc = crc32(0L, Z_NULL, 0);
-    crc = crc32(crc, (const Bytef*)pkt_bytes , sizeof(pkt_t)-4);
+    crc = crc32(crc, (const Bytef*)pkt_bytes , sizeof(pkt)-4);
     //Buf = concat du flux et du crc calculé
-    memcpy(buf, strcat(pkt_bytes,(char*)crc), sizeof(pkt_t));
-    *len = sizeof(pkt_t);
+    memcpy(buf, pkt_bytes, sizeof(pkt)-4);
+    memcpy(buf, (char*)crc, 4);
+    *len = sizeof(buf);
     free(pkt_bytes);
     return PKT_OK;
 }
@@ -151,7 +205,7 @@ const char* pkt_get_payload(const pkt_t*pkt)
 pkt_status_code pkt_set_type(pkt_t *pkt, const ptypes_t type)
 {
     /* Your code will be inserted here */
-    if (type != PTYPE_ACK || type != PTYPE_DATA || type != PTYPE_NACK) {
+    if (type != PTYPE_ACK && type != PTYPE_DATA && type != PTYPE_NACK) {
         return E_TYPE;
     }
     pkt->type = type;
@@ -186,13 +240,15 @@ pkt_status_code pkt_set_payload(pkt_t *pkt,
                                 const char *data,
                                 const uint16_t length)
 {
-    char* payload = (char*)malloc(pkt->length);
-    strcat(payload, data);
     uint padding = length % 4;
-    while (padding > 0){
-        strcat(payload, NULL);
-        padding--;
+    char* payload = (char*)malloc(pkt->length+padding);
+    memcpy(payload, data, length);
+    uint i = 1;
+    while (i <= padding){
+        memcpy(&payload[length+i], "\0", sizeof(uint));
+        i++;
     }
+    pkt_set_length(pkt,length+padding);
     memcpy(pkt->payload, payload, pkt->length);
     free(payload);
     return PKT_OK;
