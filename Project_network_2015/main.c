b9175033bbe829a596f85687603e33552d0a0a32
Geoffroy Zoetardt <geoffroyzoetardt@wifi-studentl-6031.sri.ucl.ac.be>
2015-10-05 20:33:03 +0200

    probleme malloc

--- Project_network_2015/main.c
+++ Project_network_2015/main.c
@@ -5,13 +5,20 @@
 //  Created by Geoffroy Zoetardt on 5/10/15.
 //  Copyright © 2015 Geoffroy Zoetardt. All rights reserved.
 //
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "main.h"
 
+size_t pkt_get_total_length(const pkt_t* pkt);
+
 int main(){
-   pkt_t* pkt = pkt_new();
-    /*pkt_set_type(pkt, PTYPE_ACK);
+    pkt_t* pkt = pkt_new();
+   // printf("%s",pkt);
+    pkt_set_type(pkt, PTYPE_ACK);
+    char* data ="data test 1      zzzzz        eeerecez";
+    pkt_set_payload(pkt, data, strlen(data));
     char buf [sizeof(pkt_t*)];
-    pkt_encode(pkt, buf, (size_t*)sizeof(pkt_t*));*/
-    printf("hello\n");
+    size_t size = pkt_get_total_length(pkt);
+    printf("out:%d\n",pkt_encode(pkt, buf, &size));
 }
